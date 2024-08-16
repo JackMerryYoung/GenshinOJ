7ebf406e55b7294b40d3078e4a181158fc53f81b
JackMerryYoung <cjj2008717@outlook.com>
2024-08-16 22:40:06 +0800

    Very little modification for rust. :(

--- rust_backend/main_backend/src/main.rs
+++ rust_backend/main_backend/src/main.rs
@@ -1,3 +1,4 @@
+use dlopen2::wrapper::WrapperApi;
 use std::io::Read;
 
 #[derive(serde::Deserialize, serde::Serialize, Debug)]
@@ -15,15 +16,18 @@ struct ModuleConfigJson {
     working_load: std::collections::HashMap<String, SingleModuleConfigJson>,
 }
 
-struct ModuleInstance {}
+#[derive(dlopen2::wrapper::WrapperApi)]
+struct ModuleInstance {
+    on_init: fn(),
+    on_unload: fn(),
+}
 
 fn main() {
-    let mut module_list: std::collections::HashMap<String, ModuleInstance> =
-        std::collections::HashMap::new();
+    let mut module_instances: Vec<dlopen2::wrapper::Container<ModuleInstance>> = vec![];
     let mut tmp: std::path::PathBuf = std::env::current_dir().unwrap();
     tmp.pop();
     let module_config_json_file_path: String =
-        String::from(tmp.to_str().unwrap()) + "/module_config.json";
+        String::from(tmp.to_str().unwrap()) + "/module_config_rs.json";
     let mut module_config_json_file: std::fs::File =
         match std::fs::File::open(&module_config_json_file_path) {
             Ok(file) => file,
@@ -40,7 +44,25 @@ fn main() {
     module_config_json_file
         .read_to_string(&mut module_config_json_string)
         .unwrap();
-    let mut module_config_json: ModuleConfigJson =
+    let module_config_json: ModuleConfigJson =
         serde_json::from_str(module_config_json_string.as_str()).unwrap();
-    println!("{:?}", module_config_json.working_load);
+    for (name, config) in module_config_json.working_load {
+        if config.enabled {
+            println!("[Info] Loading module {}...", name);
+            let module: Result<dlopen2::wrapper::Container<ModuleInstance>, dlopen2::Error> =
+                unsafe { dlopen2::wrapper::Container::load(&config.path) };
+            match module {
+                Ok(module) => {
+                    println!("[Info] Successfully loaded module {}.", name);
+                    module_instances.push(module);
+                }
+                Err(e) => {
+                    eprintln!(
+                        "[Error] Failed to load module `{}`. Maybe the module file doesn't exist or is not a valid shared object?",
+                        &config.path
+                    );
+                }
+            }
+        }
+    }
 }
