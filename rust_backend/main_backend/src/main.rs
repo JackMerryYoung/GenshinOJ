4295276bec4d4f69268ad0332e0f62f078865254
JackMerryYoung <cjj2008717@outlook.com>
2024-08-17 16:02:30 +0800

    wtf

--- rust_backend/main_backend/src/main.rs
+++ rust_backend/main_backend/src/main.rs
@@ -1,12 +1,27 @@
 use dlopen2::wrapper::WrapperApi;
 use std::io::Read;
 
+struct Holder {
+    func: Box<dyn Fn() -> futures::future::BoxFuture<'static, ()>>
+}
+
+impl Holder {
+    fn new<F>(f: fn() -> F) -> Holder where F: futures::future::Future<Output = ()> + Send + 'static {
+        Holder {
+            func: Box::new(move || Box::pin(f())),
+        }
+    }
+
+    async fn run(&self) {
+        (self.func)().await;
+    }
+}
+
 #[derive(serde::Deserialize, serde::Serialize, Debug)]
 struct SingleModuleConfigJson {
     name: String,
     id: String,
     enabled: bool,
-    path: String,
     dependencies: Vec<String>,
     unload_timeout: usize,
 }
@@ -14,32 +29,35 @@ struct SingleModuleConfigJson {
 #[derive(serde::Deserialize, serde::Serialize, Debug)]
 struct ModuleConfigJson {
     working_load: std::collections::HashMap<String, SingleModuleConfigJson>,
+    restricted_mode: bool,
 }
 
 #[derive(dlopen2::wrapper::WrapperApi)]
 struct ModuleInstance {
-    on_init: fn(),
-    on_unload: fn(),
+    on_init: extern "Rust" fn() -> core::future::Future<Output = ()>,
+    on_unload: extern "Rust" fn() -> core::future::Future<Output = ()>,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let mut module_instances: Vec<dlopen2::wrapper::Container<ModuleInstance>> = vec![];
     let mut tmp: std::path::PathBuf = std::env::current_dir().unwrap();
     tmp.pop();
     let module_config_json_file_path: String =
         String::from(tmp.to_str().unwrap()) + "/module_config_rs.json";
-    let mut module_config_json_file: std::fs::File =
-        match std::fs::File::open(&module_config_json_file_path) {
-            Ok(file) => file,
-            Err(e) => {
-                eprintln!(
-                "[Error] Failed to open module_config.json(`{}`). Maybe the file doesn't exist?",
+    let mut module_config_json_file: std::fs::File = match std::fs::File::open(
+        &module_config_json_file_path,
+    ) {
+        Ok(file) => file,
+        Err(e) => {
+            eprintln!(
+                "[MAIN_BACKEND] [Error] Failed to open module_config_rs.json from `{}`. Maybe the file doesn't exist?",
                 module_config_json_file_path
             );
-                eprintln!("[Error] {}", e);
-                return;
-            }
-        };
+            eprintln!("[MAIN_BACKEND] [Error] {}", e);
+            return;
+        }
+    };
     let mut module_config_json_string: String = String::new();
     module_config_json_file
         .read_to_string(&mut module_config_json_string)
@@ -48,19 +66,32 @@ fn main() {
         serde_json::from_str(module_config_json_string.as_str()).unwrap();
     for (name, config) in module_config_json.working_load {
         if config.enabled {
-            println!("[Info] Loading module {}...", name);
+            println!("[MAIN_BACKEND] [Info] Loading module {}...", name);
+            let path: String = String::from(tmp.to_str().unwrap())
+                + "/rust_backend/modules/"
+                + &config.id
+                + "/lib"
+                + &config.id
+                + ".so";
             let module: Result<dlopen2::wrapper::Container<ModuleInstance>, dlopen2::Error> =
-                unsafe { dlopen2::wrapper::Container::load(&config.path) };
+                unsafe { dlopen2::wrapper::Container::load(&path) };
             match module {
                 Ok(module) => {
-                    println!("[Info] Successfully loaded module {}.", name);
+                    println!(
+                        "[MAIN_BACKEND] [Info] Successfully loaded module `{}` from `{}`.",
+                        name, &path
+                    );
+                    Holder::new(module.on_init).run().await;
                     module_instances.push(module);
                 }
-                Err(e) => {
+                Err(_e) => {
                     eprintln!(
-                        "[Error] Failed to load module `{}`. Maybe the module file doesn't exist or is not a valid shared object?",
-                        &config.path
+                        "[MAIN_BACKEND] [Error] Failed to load module `{}` from `{}`. Maybe the module file doesn't exist or is not a valid shared object?",
+                        name, &path
                     );
+                    if module_config_json.restricted_mode {
+                        panic!("[MAIN_BACKEND] [Error] Due to the restricted mode, the server backend now is shutting down.");
+                    }
                 }
             }
         }
--- rust_backend/ws_server/src/lib.rs
+++ rust_backend/ws_server/src/lib.rs
@@ -1,14 +1,34 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[no_mangle]
+async extern "Rust" fn on_init() {
+    println!("[WS_SERVER] [INFO] Initializing the Websocket server...");
+    let app: axum::Router = axum::Router::new().route("/ws", axum::routing::get(ws_handler));
+    let listener: tokio::net::TcpListener = tokio::net::TcpListener::bind("0.0.0.0:9982").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+    println!("[WS_SERVER] [INFO] Initialized the Websocket server.");
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[no_mangle]
+async extern "Rust" fn on_unload() {
+    println!("[WS_SERVER] [INFO] Unloading the Websocket server...");
+    println!("[WS_SERVER] [INFO] Unloaded the Websocket server.");
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+async fn ws_handler(ws: axum::extract::ws::WebSocketUpgrade) -> axum::response::Response {
+    ws.on_upgrade(ws_handle_socket)
+}
+
+async fn ws_handle_socket(mut socket: axum::extract::ws::WebSocket) {
+    while let Some(msg) = socket.recv().await {
+        let msg = if let Ok(msg) = msg {
+            msg
+        } else {
+            // client disconnected
+            return;
+        };
+
+        if socket.send(msg).await.is_err() {
+            // client disconnected
+            return;
+        }
     }
 }
